ea7de31030f9b92356ddefacacac9cdbd407b45d
Rafael Vargas <rafael.s.vargas18@gmail.com>
2022-05-26 17:29:39 -0300

    Adding constructor and calculateComponentsQuant functions and resolving conflicts

--- MatrixReader.h
+++ MatrixReader.h
@@ -1,62 +1,91 @@
 #include "dataStructures/DDLinkedList.h"
+#include "iostream"
 
 using namespace structures;
-
-struct Point {
-  int row;
-  int column;
-};
+using namespace std;
 
 class MatrixReader {
-public:
-  MatrixReader(DDLinkedList<DDLinkedList<int> *> *&matrix) {
-    __matrix = matrix;
-    __componentsQuant = calculateComponentsQuant();
-  };
-  ~MatrixReader();
-  int getComponentsQuant() { return __componentsQuant; }
+    public:
+        MatrixReader(DDLinkedList<DDLinkedList<int>*>*  &matrix) {
+            __matrix = matrix;
+            __rows = matrix->size();
+            __columns = matrix->at(0)->size();
+            __componentsQuant = calculateComponentsQuant();
+            __closed_nodes = DDLinkedList<Point>();
+        };
+        ~MatrixReader();
+        int getComponentsQuant() {
+            return __componentsQuant;
+        }
+    private:
+        class Point {
+            public:
+                Point(int row, int column) {
+                    __row = row;
+                    __column = column;
+                }
+                bool operator!=(Point otherPoint) {
+                    if (__row != otherPoint.__row || __column != otherPoint.__column) return true;
+                    return false;
+                }
+                int __row;
+                int __column;
+        };
 
-private:
-  int __componentsQuant;
-  DDLinkedList<DDLinkedList<int> *> *__matrix;
-  int calculateComponentsQuant() {
-    // Upgrade
-    return 0;
-  };
-  void processPoint(struct Point &point){
-      // Upgrade
-  };
-  DDLinkedList<Point> getActiveNeighbors(struct Point &point) {
-    // Upgrade
+        int __componentsQuant;
+        int __rows;
+        int __columns;
+        DDLinkedList<Point> __closed_nodes;
+        DDLinkedList<DDLinkedList<int>*>*  __matrix;
+        int calculateComponentsQuant() {
+            int quant = 0;
+            for (size_t i = 0; i < __rows; i++) {
+                for (size_t j = 0; j < __columns; j++) {
+                    Point point = Point(i, j);
+
+                    if (!__closed_nodes.contains(point)) {
+                        processPoint(point);
+                        quant++;
+                    }
+                }
+            }
+            return quant;
+        };
+        void processPoint(Point &point) {
+            // Upgrade
+        };
+
+  DDLinkedList<Point> getActiveNeighbors(Point &point) {
     DDLinkedList<Point> lista;
-    struct Point side_positions[4];
-    side_positions[0].row = point.row + 1;
-    side_positions[0].column = point.column;
-    side_positions[1].row = point.row;
-    side_positions[1].column = point.column + 1;
-    side_positions[2].row = point.row;
-    side_positions[2].column = point.column - 1;
-    side_positions[4].row = point.row - 1;
-    side_positions[4].column = point.column;
+
+    Point* side_positions[4];
+    side_positions[0]->__row = point.__row + 1;
+    side_positions[0]->__column = point.__column;
+    side_positions[1]->__row = point.__row;
+    side_positions[1]->__column = point.__column + 1;
+    side_positions[2]->__row = point.__row;
+    side_positions[2]->__column = point.__column - 1;
+    side_positions[4]->__row = point.__row - 1;
+    side_positions[4]->__column = point.__column;
 
     for (int i = 0; i < 4; i++) {
-      if (isValidPoint(side_positions[i]) && isActivePoint(side_positions[i])) {
-        lista.push_back(side_positions[i]);
-      }
+        if (isValidPoint(*side_positions[i]) && isActivePoint(*side_positions[i])) {
+            lista.push_back(*side_positions[i]);
+        }
     }
 
     return lista;
   };
-  bool isActivePoint(struct Point &point) {
-    // Upgrade
-    return __matrix->at(point.row)->at(point.column) == 1;
+  bool isActivePoint(Point &point) {
+    return __matrix->at(point.__row)->at(point.__column) == 1;
   };
-  bool isValidPoint(struct Point &point) {
-    if (point.row < 0 || point.row >= __rows) {
+
+  bool isValidPoint(Point &point) {
+    if (point.__row < 0 || point.__row >= __rows) {
       return false;
     }
 
-    if (point.column < 0 || point.column >= __columns) {
+    if (point.__column < 0 || point.__column >= __columns) {
       return false;
     }
 
--- main.cpp
+++ main.cpp
@@ -6,13 +6,16 @@ int main(int argc, char const *argv[])
     DDLinkedList<DDLinkedList<int>*>* lista = new DDLinkedList<DDLinkedList<int>*>();
     DDLinkedList<int>* row1 = new DDLinkedList<int>();
     DDLinkedList<int>* row2 = new DDLinkedList<int>();
+    DDLinkedList<int>* row3 = new DDLinkedList<int>();
 
     for (int i = 0; i < 2; i++) {
         row1->push_back(i);
         row2->push_back(i);
+        row3->push_back(i);
     }
     lista->push_back(row1);
     lista->push_back(row2);
+    lista->push_back(row3);
     
     MatrixReader* reader = new MatrixReader(lista);
     lista->clear();
