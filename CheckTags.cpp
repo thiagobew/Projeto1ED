b9a34db6d36d74d0eb5f506bc510cd620441310c
Rafael Vargas <rafael.s.vargas18@gmail.com>
2022-06-17 23:11:36 -0300

    Sending checkAlinhamento to specific file

--- CheckTags.cpp
+++ CheckTags.cpp
@@ -1,7 +1,21 @@
 #include "dataStructures/LinkedStack.h"
 #include <cstring>
-
+#include <fstream>
+#include <iostream>
+#include <string>
 using namespace structures;
+using namespace std;
+
+void extractWordToChar(char *texto, int start, int size, char *outPalavra) {
+    int real_index = start;
+    for (int i = 0; i < size; i++) {
+        // BIZARRO: Esses dois prints dão segmentation fault, mesmo eu usando essas variáveis
+        // printf("Char: %s\n", texto[real_index]);
+        // printf("Char: %s\n", outPalavra[i]);
+        outPalavra[i] = texto[real_index];
+        real_index++;
+    }
+}
 
 int getTagSize(const char *texto, int max_len, int start) {
     int i = start;
@@ -14,46 +28,60 @@ int getTagSize(const char *texto, int max_len, int start) {
     return size;
 }
 
-int checkAlinhamento(const char *texto) {
-    char *topo;
+int checkAlinhamento(char *texto, int length) {
     int erro = 0;
     LinkedStack<char *> *stack = new LinkedStack<char *>();
-    int length = strlen(texto);
 
     for (int i = 0; i < length - 1 && erro == 0; i++) {
-        // Se é uma abertura de tag
-        if (texto[i] == '<') {
-            char *charWithTag = new char[20];
-
-            int j = i + 1;
-            while (texto[j] != '>' && j < length && texto[j] != '\0') {
-                charWithTag[j] = texto[j];
-                j++;
-            }
-
-            stack->push(charWithTag);
-            // Se é uma fechadura de tag
-        } else if (texto[i] == '<' && texto[i + 1] == '/') {
+        if (texto[i] == '<' && texto[i + 1] == '/') {
+            // Verifica se é uma fechadura de tag
             if (stack->empty()) {
                 erro = 2;
             } else {
-                char *closingTag = new char[20];
-
-                int h = i + 2;
-                while (texto[h] != '>' && h < length && texto[h] != '\0') {
-                    closingTag[h] = texto[h];
-                    h++;
+                // Pega o tamanho da tag de fechamento
+                int quantLetters = getTagSize(texto, length, i + 2);
+                int sizeArray = quantLetters + 1;
+                // Inicializa os valores do array de chars
+                char closingTag[sizeArray];
+                for (int index = 0; index < sizeArray; index++) {
+                    if (index == quantLetters)
+                        closingTag[index] = '\0';
+                    else
+                        closingTag[index] = 'a';
                 }
+                // Extrai a tag do texto
+                extractWordToChar(texto, i + 2, quantLetters, closingTag);
 
-                topo = stack->pop();
-                if (strcmp(topo, closingTag) != 0) {
+                // Testa se a tag bate com a ultima inserida
+                char *topTag = stack->pop();
+                if (strcmp(topTag, closingTag) != 0) {
+                    printf("Tags diferentes %s e %s", topTag, closingTag);
                     erro = 1;
                 }
-                delete closingTag;
+                printf("Fechando Tag: %s\n", closingTag);
             }
+        } else if (texto[i] == '<') {
+            // Verifica se é uma abertura de tag
+            // Pega o tamanho da tag atual
+            int quantLetters = getTagSize(texto, length, i + 1);
+            int sizeArray = quantLetters + 1;
+
+            // Inicializa o array de chars
+            char openingTag[sizeArray];
+            for (int index = 0; index < sizeArray; index++) {
+                if (index == quantLetters)
+                    openingTag[index] = '\0';
+                else
+                    openingTag[index] = 'a';
+            }
+            // Extrai a tag para o char criado
+            extractWordToChar(texto, i + 1, quantLetters, openingTag);
+            printf("Abrindo Tag: %s\n", openingTag);
+            stack->push(openingTag);
         }
     }
     if (erro == 0 && !stack->empty()) {
+        cout << stack->top() << endl;
         erro = 3;
     }
 
@@ -61,5 +89,3 @@ int checkAlinhamento(const char *texto) {
     delete stack;
     return erro;
 }
-
-int main() { return 0; }
--- main.cpp
+++ main.cpp
@@ -1,137 +1,43 @@
-#include "MatrixReader.h"
-#include "dataStructures/LinkedStack.h"
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <string>
+#include "CheckTags.cpp"
+
 using namespace structures;
 using namespace std;
 
-void extractWordToChar(char *texto, int start, int size, char *outPalavra) {
-    int real_index = start;
-    for (int i = 0; i < size; i++) {
-        // BIZARRO: Esses dois prints dão segmentation fault, mesmo eu usando essas variáveis
-        // printf("Char: %s\n", texto[real_index]);
-        // printf("Char: %s\n", outPalavra[i]);
-        outPalavra[i] = texto[real_index];
-        real_index++;
-    }
-}
-
-void initializeArray(char *array, int size) {
-    for (int index = 0; index < size; index++) {
-        if (index == size - 1)
-            array[index] = '\0';
-        else
-            array[index] = 'a';
-    }
-}
-
-int getTagSize(const char *texto, int max_len, int start) {
-    int i = start;
-    int size = 0;
-    while (texto[i] != '>' && i < max_len && texto[i] != '\0') {
-        size++;
-        i++;
-    }
-
-    return size;
-}
-
-int checkAlinhamento(char *texto, int length) {
-    char *topo;
-    int erro = 0;
-    LinkedStack<char *> *stack = new LinkedStack<char *>();
-
-    for (int i = 0; i < length - 1 && erro == 0; i++) {
-        if (texto[i] == '<' && texto[i + 1] == '/') {
-            // Verifica se é uma fechadura de tag
-            if (stack->empty()) {
-                erro = 2;
-            } else {
-                // Pega o tamanho da tag de fechamento
-                int quantLetters = getTagSize(texto, length, i + 2);
-                int sizeArray = quantLetters + 1;
-                // Inicializa os valores do array de chars
-                char closingTag[sizeArray];
-                for (int index = 0; index < sizeArray; index++) {
-                    if (index == quantLetters)
-                        closingTag[index] = '\0';
-                    else
-                        closingTag[index] = 'a';
-                }
-                // Extrai a tag do texto
-                extractWordToChar(texto, i + 2, quantLetters, closingTag);
-
-                printf("Tag de fechadura: %s\n", closingTag);
-                topo = stack->pop();
-                if (strcmp(topo, closingTag) != 0) {
-                    printf("Tags diferentes %s e %s", topo, closingTag);
-                    erro = 1;
-                }
-            }
-        } else if (texto[i] == '<') {
-            // Verifica se é uma abertura de tag
-            // Pega o tamanho da tag atual
-            int quantLetters = getTagSize(texto, length, i + 1);
-            int sizeArray = quantLetters + 1;
-
-            // Inicializa o array de chars
-            char openingTag[sizeArray];
-            for (int index = 0; index < sizeArray; index++) {
-                if (index == quantLetters)
-                    openingTag[index] = '\0';
-                else
-                    openingTag[index] = 'a';
-            }
-            // Extrai a tag para o char criado
-            extractWordToChar(texto, i + 1, quantLetters, openingTag);
-
-            printf("Tag de abertura: %s\n", openingTag);
-            stack->push(openingTag);
-        }
-    }
-    if (erro == 0 && !stack->empty()) {
-        cout << stack->top() << endl;
-        erro = 3;
-    }
-
-    stack->clear();
-    delete stack;
-    return erro;
-}
-
 int main() {
-
     char xmlfilename[100];
-    fstream newfile;
 
+    fstream xlmFile;
     // Copyrightstd::cin >> xmlfilename;  // entrada
+    xlmFile.open("dataset1.xml", ios::in);
 
-    newfile.open("dataset1.xml", ios::in);
-
+    // Le arquivo xml
     string texto;
-    if (newfile.is_open()) {
+    if (xlmFile.is_open()) {
         string line;
-
-        while (getline(newfile, line)) {
+        while (getline(xlmFile, line)) {
             texto += line;
         }
-
         cout << texto << endl;
-        newfile.close();
+        xlmFile.close();
     }
 
-    char *paraCheckAlinhamento = new char[texto.length()];
+    // Conversão de string para char*
+    char *inputText = new char[texto.length()];
     for (int i = 0; i < texto.length(); i++) {
-        paraCheckAlinhamento[i] = texto[i];
+        inputText[i] = texto[i];
     }
 
-    int valido = checkAlinhamento(paraCheckAlinhamento, texto.length());
+    // Verifica se está corretamente alinhado
+    int hasError = checkAlinhamento(inputText, texto.length());
+    printf("Error Quant = %d\n", hasError);
+    if (hasError == 0) {
+        printf("XML válido!\n");
+    } else {
+        return 0;
+        printf("XML inválido!\n");
+    }
 
-    cout << "Erro: " << valido << endl;
-
-    delete paraCheckAlinhamento;
+    delete inputText;
     return 0;
 }
 
